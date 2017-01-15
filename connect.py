0b46a2401a3d574329874487a6460a00701d4bde
PostgreSQL administrator <postgres@hackathon.localdomain>
2017-01-15 03:22:08 +0000

    Adding minmax polygonization and hotspot centroid algorithms

--- connect.py
+++ connect.py
@@ -11,11 +11,12 @@ def main():
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
 
-    services = ["ride", "car", "food", "mart", "send", "glam", "tix", "massage", "auto", "pulsa"]
+    #services = ["ride", "car", "food", "mart", "send", "glam", "tix", "massage", "auto", "pulsa"]
+    services=["ride","glam","car","food"]
     y=int(sys.argv[3])
     for x in range(0,y):
     	mypoint1 = "("+ str(randint(1,25000))+ ",ST_MakePoint("+sys.argv[1]+"."+str(randint(10,99))+str(randint(10,99))+","+str(sys.argv[2])+"."+ str(randint(10, 99)) + str(randint(10, 99))+")"
-    	mypoint2 = mypoint1 + ",'"+ str(services[randint(0,9)])+"',"+str(random.choice([True, False])) + ");"
+    	mypoint2 = mypoint1 + ",'"+ str(services[randint(0,3)])+"',"+str(random.choice([True, False])) + ");"
     	query = "insert into gohack(userid, location, service, isbooked) values"+str(mypoint2).strip(' \t\n\r')    
 	cursor.execute(str(query))
 
--- geoclusters_clean.py
+++ geoclusters_clean.py
@@ -1,7 +1,7 @@
 # To find hot-spots based on location data
 
 import sys
-import psycopg2  
+import psycopg2
 import numpy as np
 import scipy.cluster.hierarchy as hcluster
 from math import cos, asin, sqrt
@@ -21,7 +21,7 @@ def distance(loc1, loc2):
 def distance_matrix(loc_array):
     # compute distance between 2 arrays of lat-long co-ordinates & returns square matrix
     dis_matrix = np.empty([len(loc_array), len(loc_array)], dtype=float)
-    
+
     for i in xrange(len(loc_array)):
         for j in xrange(len(loc_array)):
             dis_matrix[i][j] = distance(loc_array[i],loc_array[j])
@@ -44,63 +44,22 @@ def poly_edges(points):
 
 # main function
 
-# copy latlong data
-#csv_file = 'TwoClusterswithNoise.csv'
-#df = pd.read_csv(csv_file)
-#latArray = df['lat'].values
-#lonArray = df['lon'].values
-#locArray = np.array((latArray, lonArray)).T
-#bookArray = df['booking'].values
-
 conn_string = "host='localhost' dbname='gojek' user='postgres' password='karthi'"
 conn = psycopg2.connect(conn_string)
 curs = conn.cursor()
-
 curs.execute("select ST_X(location),ST_Y(location) from gohack as g where service='"+sys.argv[1]+"' and isbooked='t' and  g.loggedtime > current_timestamp -interval '30 minutes';")
 locArray = curs.fetchall()
 
-#conn.close()
-
-#print distance_matrix(locArray)
-
-#print df
-
-# clustering
-#thresh = 0.05
-thresh = 1.5
+thresh = 0.005
 clusters = np.array(hcluster.fclusterdata(locArray, thresh, criterion="distance"), dtype=int)
-#clusters = hcluster.fclusterdata(distance_matrix(locArray), thresh, criterion="distance")
-#df['cluster_id'] = clusters
-
-#print clusters
-#print df
-#cluster_ids = clusters.astype(np.int64)
-#np.savetxt('clusters.txt', clusters)
-
 hotspot = np.bincount(clusters).argmax()
-#print hotspot
-
 indices = [i for i, j in enumerate(clusters) if j == hotspot]
-#print indices
-
-#print len(clusters)
-#print len(indices)
 
 hotspot_points = np.array([locArray[i] for i in indices])
 print "Current demand in hotspot: ", len(hotspot_points)
 
 centeroid = centeroidnp(hotspot_points)
-print centeroid
+print "Centeroid of hotspot: ", centeroid
 
 edges = poly_edges(hotspot_points)
-print edges
-
-# plotting
-#x = np.arange(10)
-#plt.scatter(*numpy.transpose(locArray), s=30, c=clusters)
-#plt.axis("equal")
-#title = "threshold: %f, number of clusters: %d" % (thresh, len(set(clusters)))
-#plt.title(title)
-#plt.show()
-
-
+print "Edges of hotspot: ", edges
